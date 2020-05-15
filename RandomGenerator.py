3fe8b3abb93d57888520af330b4d75f376d23f74
Martin <pereyramartinn@gmail.com>
2020-05-14 22:11:19 -0300

    refactor lcg function, bitmap solved

--- RandomGenerator.py
+++ RandomGenerator.py
@@ -1,5 +1,6 @@
 import pylab as plt
 import numpy as np
+import scipy as sci
 
 
 # LCG Implementation
@@ -11,25 +12,33 @@ def lcg(a, c, m, initial_seed):
         seed = rand
         yield rand
 
-def random_sample(n, interval, seed = 20200420162000):
-    lower, upper = interval[0], (interval[1]+1)
+def random_sample(n, seed = 1103603443773377):
     sample = []
-    varAux = lcg(7**5, 5**7, 2**32, seed)
+    global sum
+    varAux = lcg(1103590199, 419329, (2**32), seed)
     for i in range(n):
-        observation = (upper - lower) * (next(varAux) / ((2**32)-1)) + lower
-        sample.append(int(observation))
+        observation = next(varAux) / (2**32)
+        sum += observation
+        sample.append(observation)
 
     return sample
 
+#Test to LCG
 
-Z = []
-for i in range(501):
-    Z.append(random_sample(500, [0, 1]))
+def
 
 
-plt.imshow(Z, cmap='gray', interpolation='nearest')
-plt.show()
 
-U = np.random.random((500, 500))   # Test data
-plt.imshow(U, cmap='gray', interpolation='nearest')
-plt.show()
+
+
+
+
+# Bitmap plot of LGC and numpy random
+def plot_bitmap():
+    Z = np.array(random_sample(250000)).reshape((500,500))
+    plt.imshow(Z, cmap='gray', interpolation='nearest')
+    plt.show()
+
+    U = np.random.random((500, 500))   # Test data
+    plt.imshow(U, cmap='gray', interpolation='nearest')
+    plt.show()
