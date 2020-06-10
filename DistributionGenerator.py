08f54d0d55d4c5dc55c9c6f51f6969fba96b22ed
Martin <pereyramartinn@gmail.com>
2020-06-09 22:28:57 -0300

    geometric distribution

--- DistributionGenerator.py
+++ DistributionGenerator.py
@@ -1,6 +1,3 @@
-import random as rd
-
-from RandomGenerator import random_sample
 import math as mt
 import numpy as np
 from scipy import special
@@ -56,6 +53,26 @@ def plotting_poisson(lambd, poissonValues):
     plt.show()
 
 
+def generate_geometric_values(p, size):
+    geometricValuesList = []
+    for i in range(size):
+        r = rd.random()
+        q = 1 - p
+        x = mt.floor(mt.log(r) / mt.log(q))
+        geometricValuesList.append(x)
+    return geometricValuesList
+
+
+def plotting_geometric(p, geometricValues):
+    sns.distplot(geometricValues, kde=False, color="b")
+    plt.show()
+
+    t = np.arange(0, 20, 0.1)
+    d = p*(np.power((1-p), t))
+
+    plt.plot(t, d)
+    plt.show()
+
 if __name__ == "__main__":
     sample_size = 10000
     mu = 0
@@ -66,3 +83,7 @@ if __name__ == "__main__":
     lambd = 5
     poissonValues = generate_poisson_values(lambd, sample_size)
     plotting_poisson(lambd, poissonValues)
+    p = 0.3
+    geometricValues = generate_geometric_values(p, sample_size)
+    print(np.mean(geometricValues))
+    # plotting_geometric(p, geometricValues)
